25b058fd94fe109b82a4a23db8ab23c201934d9f
Rohail Kabani <rohail.kabani@gmail.com>
2018-02-06 00:51:36 -0500

    get json data

--- app/src/main/java/com/rohailkabani/photoify/GetJsonData.java
+++ app/src/main/java/com/rohailkabani/photoify/GetJsonData.java
@@ -1,7 +1,12 @@
 package com.rohailkabani.photoify;
 
+import android.net.Uri;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -35,5 +40,41 @@ class GetJsonData implements GetData.OnDownloadComplete {
         String destinationUri = createUri(searchCriteria, language, matchAll);
         GetData getData = new GetData(this);
         getData.execute(destinationUri);
+        Log.d(TAG, "executeOnSameThread: Ends.");
+    }
+
+    private String createUri (String searchCriteria, String lang, boolean matchAll) {
+        Log.d(TAG, "createUri: Starts.");
+        return Uri.parse(baseURL).buildUpon()
+                .appendQueryParameter("tags", searchCriteria)
+                .appendQueryParameter("tagmode", matchAll ? "ALL" : "ANY")
+                .appendQueryParameter("lang", lang)
+                .appendQueryParameter("format", "json")
+                .appendQueryParameter("nojsoncallback", "1")
+                .build().toString();
+    }
+
+    @Override
+    public void OnDownloadComplete(String data, DOWNLOAD_STATUS status) {
+        Log.d(TAG, "OnDownloadComplete: Starting.");
+
+        if (status == DOWNLOAD_STATUS.OK) {
+            photoList = new ArrayList<>();
+            try {
+                JSONObject jsonObject = new JSONObject();
+                JSONArray jsonArray = jsonObject.getJSONArray("items");
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonPhoto = jsonArray.getJSONObject(i);
+                    String jsonTitle = jsonPhoto.getString("title");
+                    String jsonAuthor = jsonPhoto.getString("author");
+                    String jsonAuthorID = jsonPhoto.getString("author_id");
+                    
+
+
+                }
+                
+            }
+        }
     }
 }
